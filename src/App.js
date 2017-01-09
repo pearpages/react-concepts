cfca2753298199e9fe8e79899203e73509748bf4
pearpages <pere@soms.cat>
2017-01-09 13:09:06 +0100

    managing state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@
 import React from 'react';
 
 class App extends React.Component {
+    constructor() {
+        super(); // so this gets the context of this component rather than the parent
+        this.state = {
+            text2: 'this is the state txt',
+            cat: 0
+        };
+    }
+
+    update ( e ) {
+        this.setState({txt: e.target.value});
+    }
 
     render() {
         let txt = this.props.txt
         // 1st way
-        return <h1 className="myclass">{txt}</h1> 
+        return (
+            <div>
+            <input type="text" onChange={this.update.bind(this)} />
+            <h1 className="myclass">{txt} - {this.state.txt} - {this.state.cat}</h1>
+            </div>
+        )
         // 2nd way, but only can create a single node with JSX
         // return React.createElement('h1', null, 'Hello World');
     }
--- src/README.md
+++ src/README.md
@@ -3,3 +3,7 @@
 ## Creating A Component
 
 ## Setting Properties On a Component
+
+## Manage Component State
+
+State is used for properties on a component that will change, versus static properties that are passed in. 
