92360c35d9d80fc0184d7cae8429d6a9b2ceac50
pearpages <pere@soms.cat>
2017-05-16 22:26:07 +0200

    filter search

--- src/myFirstApp/src/movie.js
+++ src/myFirstApp/src/movie.js
@@ -5,7 +5,7 @@ export default function Movie(props) {
 
     const {Title,Year,imdbID,Poster} = props;
     return (<div className="movie-card">
-        <h4>{Title} <span>Year: {Year}</span></h4>
+        <h4>{Title} <span>(Year: {Year})</span></h4>
         <p>imdbID: {imdbID}</p>
         <img src={Poster} alt={Title} />
     </div>);
--- src/myFirstApp/src/search.js
+++ src/myFirstApp/src/search.js
@@ -4,15 +4,33 @@ import Movie from './movie';
 
 export default class Search extends React.Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchTerm: 'this is the default string'
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({searchTerm: event.target.value});
+    }
+
+    getMovies() {
+        return movies.filter(function filter(movie) {
+            // return movie.Title.match( new RegExp(".*"+this.state.searchTerm+".*","gi"));
+            return movie.Title.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) > 0;
+        }.bind(this));
     }
 
     render() {
         return (
             <div className="search">
-                <h1>Search page!!</h1>
-                {movies.map(function (movie) {
+                <h1>Search</h1>
+                <div>
+                    Filter: <input onChange={this.handleChange} value={this.state.searchTerm} type="text" placeholder="Filter"/>
+                </div>
+                {this.getMovies().map(function (movie) {
                     return (<Movie key={movie.imdbID} {...movie} />);
                 })}
             </div>
