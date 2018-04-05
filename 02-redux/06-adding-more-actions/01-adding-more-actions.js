134a376997ec979c244d69b10d93f3f31b2955d6
pearpages <pere@soms.cat>
2018-04-05 14:43:24 +0200

    feat: more actions

---
+++ 02-redux/06-adding-more-actions/01-adding-more-actions.js
@@ -0,0 +1,85 @@
+/*
+{
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    complete: false,
+  }
+}
+
+{
+  type: 'REMOVE_TODO',
+  id: 0,
+}
+
+{
+  type: 'TOGGLE_TODO',
+  id: 0,
+}
+
+{
+  type: 'ADD_GOAL',
+  goal: {
+    id: 0,
+    name: 'Run a Marathon'
+  }
+}
+
+{
+  type: 'REMOVE_GOAL',
+  id: 0
+}
+*/
+
+// Reducer function
+function todosReducer(state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo]);
+  } else if (action.type === 'REMOVE_TODO') {
+    return state.filter(todo => todo.id !== action.id);
+  } else if (action.type === 'TOGGLE_TODO') {
+    return state.map(todo => todo.id !== action.id ? todo : {
+      ...todo,
+      complete: !todo.complete
+    })
+  } else {
+    return state;
+  }
+}
+
+function createStore (reducer) {
+  // The store should have four parts
+  // 1. The state
+  // 2. Get the state.
+  // 3. Listen to changes on the state.
+  // 4. Update the state
+
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    // call reducer
+    // loop over listeners and invoke them
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  }
+
+  return {
+    getState,
+    subscribe,
+    dispatch
+  };
+}
+
+// Example
+const store = createStore(todosReducer);
