8453657dd540076935efeab4c5a22450a4e69da1
pearpages <pere@soms.cat>
2018-04-06 10:25:20 +0200

    chore: setup

--- 02-redux/03-react-app/01-adding-react/react.js
+++ 02-redux/03-react-app/01-adding-react/react.js
@@ -2,7 +2,38 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        REACT APP
+        <Todos />
+        <Goals />
+      </div>
+    );
+  }
+}
+
+function List(props) {
+  return (
+    <ul>
+      <li>LIST</li>
+    </ul>
+  );
+}
+
+class Todos extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>TODOS</h2>
+        <List />
+      </div>
+    );
+  }
+}
+
+class Goals extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>GOALS</h2>
+        <List />
       </div>
     );
   }
