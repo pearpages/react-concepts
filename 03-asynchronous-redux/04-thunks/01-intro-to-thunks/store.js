122178bcc384533701324957f98527294f8a028e
pearpages <pere@soms.cat>
2018-04-08 19:44:37 +0200

    feat: thunks

---
+++ 03-asynchronous-redux/04-thunks/01-intro-to-thunks/store.js
@@ -0,0 +1,142 @@
+const ADD_TODO = 'ADD_TODO';
+const REMOVE_TODO = 'REMOVE_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+const ADD_GOAL = 'ADD_GOAL';
+const REMOVE_GOAL = 'REMOVE_GOAL';
+const RECEIVE_DATA = 'RECEIVE_DATA';
+
+function addTodoAction (todo) {
+  return {
+    type: ADD_TODO,
+    todo
+  }
+}
+
+function removeTodoAction (id) {
+  return {
+    type: REMOVE_TODO,
+    id
+  }
+}
+
+function toggleTodoAction (id) {
+  return  {
+    type: TOGGLE_TODO,
+    id
+  }
+}
+
+function addGoalAction(goal) {
+  return {
+    type: ADD_GOAL,
+    goal
+  }
+}
+
+function removeGoalAction(id) {
+  return {
+    type: REMOVE_GOAL,
+    id
+  }
+}
+
+function receiveDataAction (todos, goals) {
+  return {
+    type: RECEIVE_DATA,
+    todos,
+    goals
+  }
+}
+
+function loading(state = true, action) {
+  switch(action.type) {
+    case RECEIVE_DATA:
+      return false;
+    default:
+      return state;
+  }
+}
+
+function handleDeleteTodo(todo) {
+  return (dispatch) => {
+    dispatch(removeTodoAction(todo.id));
+    return API.deleteTodo(todo.id)
+    .catch(() => {
+      dispatch(addTodoAction(todo));
+      alert('An error occurred. Try again.');
+    });
+  }
+}
+
+// Reducer function
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat([action.todo]);
+    case REMOVE_TODO:
+      return state.filter(todo => todo.id !== action.id);
+    case TOGGLE_TODO:
+      return state.map(todo => todo.id !== action.id ? todo : {
+        ...todo,
+        complete: !todo.complete
+      });
+    case RECEIVE_DATA:
+      return action.todos.slice();
+    default:
+      return state;
+  }
+}
+
+function goals(state = [], action) {
+  switch(action.type) {
+    case ADD_GOAL :
+      return state.concat([action.goal]);
+    case REMOVE_GOAL :
+      return state.filter(goal => goal.id !== action.id);
+    case RECEIVE_DATA:
+      return action.goals.slice();
+    default:
+      return state;
+  }
+}
+
+function checker(store) {
+  const containsBitcoin = (name) => {
+    return name.toLowerCase().indexOf('bitcoin') !== -1;
+  };
+
+  // this is called currying
+  return function (next) {
+    return function (action) {
+
+      let hasBitcoin = false;
+      switch (action.type) {
+        case ADD_TODO:
+          hasBitcoin = containsBitcoin(action.todo.name);
+        break;
+        case ADD_GOAL:
+          hasBitcoin = containsBitcoin(action.goal.name);
+        break;
+      }
+
+      return hasBitcoin ? alert('Nope. That is a bad idea.') : next(action);
+    }
+  }
+}
+
+const logger = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.log('The action: ', action);
+  const result = next(action);
+  console.log('The new state: ', store.getState());
+  console.groupEnd();
+  return result;
+};
+
+const thunk = (store) => (next) => (action) => {
+  if (typeof action == 'function') {
+    return action(store.dispatch);
+  }
+
+  return next(action);
+}
