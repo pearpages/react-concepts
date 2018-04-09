1a906d5728608400e87dca099445bd13a638e59f
pearpages <pere@soms.cat>
2018-04-10 00:19:44 +0200

    feat: middleware

--- 02-redux/04-redux-folder-structure/01-goals-todos/src/actions/shared.js
+++ 02-redux/04-redux-folder-structure/01-goals-todos/src/actions/shared.js
@@ -21,41 +21,7 @@ export function handleInitialData() {
   };
 }
 
-// middleware
-
-function checker(store) {
-  const containsBitcoin = (name) => {
-    return name.toLowerCase().indexOf('bitcoin') !== -1;
-  };
-
-  // this is called currying
-  return function (next) {
-    return function (action) {
-
-      let hasBitcoin = false;
-      switch (action.type) {
-        case ADD_TODO:
-          hasBitcoin = containsBitcoin(action.todo.name);
-        break;
-        case ADD_GOAL:
-          hasBitcoin = containsBitcoin(action.goal.name);
-        break;
-      }
-
-      return hasBitcoin ? alert('Nope. That is a bad idea.') : next(action);
-    }
-  }
-}
-
-const logger = (store) => (next) => (action) => {
-  console.group(action.type);
-  console.log('The action: ', action);
-  const result = next(action);
-  console.log('The new state: ', store.getState());
-  console.groupEnd();
-  return result;
-};
-
+// TODO: remove from here
 const store = Redux.createStore(Redux.combineReducers({
   todos,
   goals,
---
+++ 02-redux/04-redux-folder-structure/01-goals-todos/src/middleware/checker.js
@@ -0,0 +1,23 @@
+export default function checker(store) {
+  const containsBitcoin = (name) => {
+    return name.toLowerCase().indexOf('bitcoin') !== -1;
+  };
+
+  // this is called currying
+  return function (next) {
+    return function (action) {
+
+      let hasBitcoin = false;
+      switch (action.type) {
+        case ADD_TODO:
+          hasBitcoin = containsBitcoin(action.todo.name);
+        break;
+        case ADD_GOAL:
+          hasBitcoin = containsBitcoin(action.goal.name);
+        break;
+      }
+
+      return hasBitcoin ? alert('Nope. That is a bad idea.') : next(action);
+    }
+  }
+}
