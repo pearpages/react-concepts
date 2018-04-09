1e2867459e0464e2e9fb51c497908ffcf4332554
pearpages <pere@soms.cat>
2018-04-09 12:42:49 +0200

    feat: redux thunk middleware

---
+++ 03-asynchronous-redux/04-thunks/04-redux-thunk-middleware/app/goals.js
@@ -0,0 +1,30 @@
+class Goals extends React.Component {
+  input;
+  addItem = (e) => {
+    e.preventDefault();
+
+    this.props.store.dispatch(handleAddGoal(
+      this.input.value,
+      () => this.input.value = ''
+    ));
+  }
+
+  removeItem = (goal) => {
+    this.props.store.dispatch(handleDeleteGoal(goal));
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Goals List</h2>
+        <input
+          type='text'
+          placeholder='Add Goal'
+          ref={(input) => this.input = input}
+        />
+        <button onClick={this.addItem}>Add Goal</button>
+        <List items={this.props.goals} remove={this.removeItem} />
+      </div>
+    );
+  }
+}
---
+++ 03-asynchronous-redux/04-thunks/readme.md
@@ -0,0 +1,4 @@
+# Read/Play with after the Code
+
+* [Redux Promise](https://github.com/redux-utilities/redux-promise) - FSA-compliant promise middleware for Redux.
+* [Redux Saga](https://github.com/redux-saga/redux-saga) - An alternative side effect model for Redux apps
