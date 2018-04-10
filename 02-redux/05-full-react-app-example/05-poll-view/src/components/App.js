28d7eb6ae0c7ff4bf859932b79446a59ca84314b
pearpages <pere@soms.cat>
2018-04-10 17:51:36 +0200

    feat: Poll component

--- 02-redux/05-full-react-app-example/05-poll-view/src/components/App.js
+++ 02-redux/05-full-react-app-example/05-poll-view/src/components/App.js
@@ -5,6 +5,7 @@ import Dashboard from './Dashboard';
 import LoadingBar from 'react-redux-loading';
 import Leaderboard from './Leaderboard';
 import AddPoll from './AddPoll';
+import Poll from './Poll';
 
 class App extends Component {
   componentDidMount() {
@@ -18,6 +19,7 @@ class App extends Component {
         {this.props.loading === true
           ? null
           : <div>
+            <Poll match={{params: {id: 'loxhs1bqm25b708cmbf3g'}}}/>
             <AddPoll />
             <Dashboard />
             <Leaderboard />
