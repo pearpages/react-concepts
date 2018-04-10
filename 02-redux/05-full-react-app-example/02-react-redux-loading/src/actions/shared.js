7ac1ea09b0b1ae659e8f842e311409aab2fbfd78
pearpages <pere@soms.cat>
2018-04-10 13:18:21 +0200

    chore: folders refacto

---
+++ 02-redux/05-full-react-app-example/02-react-redux-loading/src/actions/shared.js
@@ -0,0 +1,17 @@
+import { getInitialData } from '../utils/api';
+import { receiveUsers } from './users';
+import { receivePolls } from './polls';
+import { setAuthedUser } from './authedUser';
+
+const AUTHED_ID = 'tylermcginnis';
+
+export function handleInitialData() {
+  return (dispatch) => {
+    return getInitialData()
+      .then(({users, polls}) => {
+        dispatch(receiveUsers(users));
+        dispatch(receivePolls(polls));
+        dispatch(setAuthedUser(AUTHED_ID));
+      });
+  }
+}
