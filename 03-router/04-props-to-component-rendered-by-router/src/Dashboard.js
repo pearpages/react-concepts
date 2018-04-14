69b18e33c4e2eff090c90e0fc41e18c3b6101704
pearpages <pere@soms.cat>
2018-04-14 19:00:50 +0200

    feat: components

---
+++ 03-router/04-props-to-component-rendered-by-router/src/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        Dasbhoard
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
